c8c6b388e0e4d74bfd5aefd7eab1bdc42c4aead7
Taiga2022 <f2021212010@gmail.com>
2025-09-23 15:11:38 +0900

    fix:git_rm

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -1,5 +1,15 @@
 #ifndef PHILO_H
 #define PHILO_H
 
+#include <stddef.h>     /* size_t */
+#include <stdint.h>     /* integer types if needed */
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* malloc, free, calloc, realloc, exit */
+#include <string.h>     /* memset, memcpy, strcmp, strdup... */
+#include <unistd.h>     /* write, usleep, close, read, STDOUT_FILENO */
+#include <sys/types.h>  /* ssize_t, off_t (often included via unistd.h) */
+#include <sys/time.h>   /* gettimeofday, struct timeval */
+#include <errno.h>      /* errno */
+#include <pthread.h>    /* pthread_create/join/detach and mutex APIs */
 
 #endif //PHILO_H
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -1,5 +1,4 @@
-
-#include <stdio.h>
+#include "philo.h"
 
 int main()
 {
