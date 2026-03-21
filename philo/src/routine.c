6bca4d912393f83521ea026d0c11d3016a395261
tshimizu <f2021212010@gmail.com>
2026-03-21 23:00:58 +0900

    fix:init

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: tshimizu <tshimizu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 18:24:15 by tshimizu          #+#    #+#             */
-/*   Updated: 2026/02/22 14:04:07 by tshimizu         ###   ########.fr       */
+/*   Updated: 2026/03/21 22:57:55 by tshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,31 @@ void	print_action(t_philo *philo, char *action)
 	pthread_mutex_unlock(&(rules->print_mutex));
 }
 
-static void	init_philo_state(t_philo *philo)  
-{  
-	if (philo->id % 2 == 1)  
-		usleep(1000);  
-	pthread_mutex_lock(&(philo->meal_mutex));  
-	philo->last_meal = ft_get_timestamp();  
-	pthread_mutex_unlock(&(philo->meal_mutex));  
-	pthread_mutex_lock(&(philo->rules->ready_mutex));  
-	philo->rules->ready_count++;  
-	pthread_mutex_unlock(&(philo->rules->ready_mutex));  
+static void	init_philo_state(t_philo *philo)
+{
+	t_rules	*rules;
+	int		delay;
+
+	rules = philo->rules;
+	delay = 0;
+	if (rules->n_philo > 2 && rules->n_philo % 2 == 0)
+	{
+		if (philo->id % 2 == 1)
+			delay = rules->time_to_eat * 1000;
+	}
+	else if (rules->n_philo > 2 && rules->n_philo % 2 == 1)
+	{
+		delay = (philo->id * 2 % rules->n_philo)
+			* (rules->time_to_eat * 1000 / (rules->n_philo / 2));
+	}
+	if (delay > 0)
+		usleep(delay);
+	pthread_mutex_lock(&(philo->meal_mutex));
+	philo->last_meal = ft_get_timestamp();
+	pthread_mutex_unlock(&(philo->meal_mutex));
+	pthread_mutex_lock(&(rules->ready_mutex));
+	rules->ready_count++;
+	pthread_mutex_unlock(&(rules->ready_mutex));
 }
 
 static int	check_death(t_rules *rules)
@@ -52,13 +67,21 @@ static int	check_death(t_rules *rules)
 static void	philo_cycle(t_philo *philo)
 {
 	t_rules	*rules;
+	int		think_time;
 
 	rules = philo->rules;
 	eat(philo);
 	print_action(philo, "is sleeping");
 	ft_precise_sleep(rules->time_to_sleep);
 	print_action(philo, "is thinking");
-	usleep(500);
+	think_time = rules->n_philo * (rules->time_to_eat
+			/ (rules->n_philo / 2)) - rules->time_to_eat - rules->time_to_sleep;
+	if (think_time < 0)
+		think_time = 0;
+	if (think_time > 0)
+		ft_precise_sleep(think_time);
+	else
+		usleep(500);
 }
 
 void	*routine(void *arg)
